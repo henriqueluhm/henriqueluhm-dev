79a3953d78a7a1cbeb0704b06dfab04e4355a9c2
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2026-01-07 22:13:22 -0300

    feat: translations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 import * as motion from "motion/react-client";
 import Image from "next/image";
 import { getTranslations } from "next-intl/server";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default async function Page() {
   const t = await getTranslations();
+
   return (
-    <main className="pb-(--nav-height) md:mt-(--nav-height) flex flex-col items-center justify-center p-4">
+    <main className="mb-(--nav-height) md:mt-(--nav-height) flex flex-col items-center justify-center px-4 py-10">
       <div className="w-full max-w-160">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-10">
           <div className="flex flex-col sm:flex-row gap-6 items-center">
             <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.1, ease: "easeOut" }}
               className="shrink-0"
             >
@@ -38,6 +45,70 @@ export default async function Page() {
               </p>
             </motion.div>
           </div>
+
+          <Accordion
+            multiple
+            defaultValue={["experience", "education", "personalProjects"]}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.3, ease: "easeOut" }}
+            >
+              <AccordionItem value="experience">
+                <AccordionTrigger>{t("home.experience")}</AccordionTrigger>
+                <AccordionContent>
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, ease: "easeOut" }}
+                  >
+                    experience here
+                  </motion.div>
+                </AccordionContent>
+              </AccordionItem>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4, ease: "easeOut" }}
+            >
+              <AccordionItem value="education">
+                <AccordionTrigger>{t("home.education")}</AccordionTrigger>
+                <AccordionContent>
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, ease: "easeOut" }}
+                  >
+                    education here
+                  </motion.div>
+                </AccordionContent>
+              </AccordionItem>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.5, ease: "easeOut" }}
+            >
+              <AccordionItem value="personalProjects">
+                <AccordionTrigger>
+                  {t("home.personalProjects")}
+                </AccordionTrigger>
+                <AccordionContent>
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, ease: "easeOut" }}
+                  >
+                    projects here
+                  </motion.div>
+                </AccordionContent>
+              </AccordionItem>
+            </motion.div>
+          </Accordion>
         </div>
       </div>
     </main>
